6c607b67863a1449ce9bcb5f2c0c0c63db7d538e
Joe Wroe <joe_wroe@icloud.com>
2019-07-27 12:24:36 +0100

    [JWB-0001] Configure use of React Testing Library
    Setup
    
    JW

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
+import { render } from '@testing-library/react';
 
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders a welcome message", () => {
+	const { getByText } = render(<App />);
+	
+	const welcomeMessage = getByText("Learn React");
+
+	expect(welcomeMessage).toBeInTheDocument();
 });
